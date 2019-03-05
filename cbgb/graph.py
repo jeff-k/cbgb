5220ed96b69cba09ed9c8419fcdc009b88d785ac
jeff-k <jeff@x-0.ca>
2019-03-05 11:25:58 -0800

    implement eulerian walk

--- cbgb/graph.py
+++ cbgb/graph.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """sliding slices for strings of length k
 """
 def kmerize(seq, k=12):
@@ -7,11 +9,30 @@ def kmerize(seq, k=12):
 class Edge:
     """prototypical edge type. monoid.
     """
-    def __init__(self):
-        pass
+    def __init__(self, data=None):
+        self.data = data
 
     def update(self, other):
-        return False
+        pass
+
+    def __repr__(self):
+        if self.data is not None:
+            return "{}".format(self.data)
+        return "<Edge>"
+
+
+class Multiedge(Edge):
+    """weighted edge for a multigraph
+    """
+    def __init__(self):
+        self.count = 1
+
+    def update(self, other):
+        self.count += other.count
+
+    def __repr__(self):
+        return "<Edge: {}>".format(self.count)
+
 
 class LMG(Edge):
     """carrier type for labelled multigraph
@@ -30,37 +51,74 @@ class LMG(Edge):
     def __repr__(self):
         return "<{} {}>".format(self.labels, self.count)
 
+
 class CdB:
     """main character
     """
     def __init__(self):
         self.edges = {}
-        self.nodes = set([])
+        self.nodes = {}
 
     def add(self, kmer, edge):
-        left, right = kmer[1:], kmer[:-1]
-        self.nodes.add(left)
-        self.nodes.add(right)
+        left, right = kmer[:-1], kmer[1:]
+
+        if left not in self.nodes:
+            self.nodes[left] = []
+
+        self.nodes[left].append(right)
 
         if (left, right) not in self.edges:
-            self.edges[(left, right)] = LMG()
-        self.edges[(left, right)].update(edge)
+            self.edges[(left, right)] = edge
+        else:
+            self.edges[(left, right)].update(edge)
 
-    def walk(self):
+    def walk(self, start=None):
         """eulerian walk!
         """
         path = [] # the path of edges
+
+        if start is None:
+            start = list(self.nodes.keys())[0]
+
+        def visit(node):
+            while len(self.nodes[node]) > 0:
+                right = self.nodes[node].pop()
+                path.append(self.edges[(node, right)])
+                if right not in self.nodes:
+                    print("broken loop; not eulerian cycle?")
+                    break # if there's a broken loop
+                visit(right)
+        visit(start)
         return path
 
     def compress(self):
         pass
 
+    def to_adj(self):
+        """dump graph as adjacency matrix
+        """
+        return np.array([[]])
+
     def to_gfa(self):
         pass
 
-    def subdag(self, start, end):
+    def subdawg(self, start, end):
         """walk from start to end, breadth first
+
+        idea: remove all in-edges to start and out edges from end, add edges
+        between start and end with the correct degree. Start walk from start to
+        end. Use it to contruct partial order alignments from
         """
+
+        # remove nodes entering start and leaving end
+        for (left, right) in self.edges.keys():
+            if right is start or left is end:
+                self.edges.pop((left, right), None)
+        self.edges[(start, end)] = None
+
+        # create contrived seam
+        for kmer in kmerize(start + end):
+            self.add(kmer, Edge(data=kmer[-1]))
         pass
 
 def test(seq1, seq2):
@@ -72,9 +130,25 @@ def test(seq1, seq2):
 
     return g.edges
 
+def assemble(kmers):
+    c = CdB()
+    for kmer in kmers:
+        c.add(kmer, Edge(data=kmer[-1]))
+    return list(map(str, c.walk()))
+
 if __name__ == "__main__":
-    g = test("ATCGACTACGACTGCTGCATCGATCGATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTACGTACGATCGATCGATCG",
-            "ATCTACTTTTTTTTTTTTTTCGCGGCAGCGATCATCGATCGACGACGATACGATCGATCGACTCGATCGATCGACCGGGGGGGGGGGGGGGACACTCGATCGATCGATCGATCACTACGATGCATGCATGCAT")
-    for k in g:
-        print(g[k])
-    print(g)
+    #g = test("ATCGACTACGACTGCTGCATCGATCGATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTACGTACGATCGATCGATCG",
+    #        "ATCTACTTTTTTTTTTTTTTCGCGGCAGCGATCATCGATCGACGACGATACGATCGATCGACTCGATCGATCGACCGGGGGGGGGGGGGGGACACTCGATCGATCGATCGATCACTACGATGCATGCATGCAT")
+    #for k in g:
+    #    print(g[k])
+    #print(g)
+
+    c = CdB()
+    rando = "TAGTGAATCGAAGCGCGGCTTCAGAATACCGTTTTGGCTACCTGATACAAAGCCCATCGTGGTCCTCAGATATCGTGCACGTAGAGTTGCACCGCACGCATGTGGAATTAGTGGCGAAGTACGATTCCAAGACCGACGTACGATACAACTATGCGGATGTGACGAGCTTCTTTTATATGCTTCGCCCGCCGGACCGGCCT"
+    for kmer in kmerize(rando):
+        c.add(kmer, Edge(data=kmer[-1]))
+    c.subdawg(rando[:12], rando[-12:])
+    print(''.join(map(str, c.walk())))
+    print(''.join(assemble(kmerize("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))))
+    randoloop = "TAGTGAATCGAAGCGCGGCTTCAGAATACCGTTTTGGCTACCTGATACAAAGCCCATCGTGGTCCTCAGATATCGTGCACGTAGAGTTGCACCGCACGCATGTGGAATTAGTGGCGAAGTACGATTCCAAGACCGACGTACGATACAACTATGCGGATGTGACGAGCTTCTTTTATATGCTTCGCCCGCCGGACCGGCCTTAGTGAATCGAAGCGCGGC"
+    print(''.join(assemble(kmerize(randoloop))))
